423d21846337fa4d3a70be0b3577d9ed54233b43
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-09-11 20:17:20 +0200

    FORK-ONLY: use bats dependencies from hardened forks

--- update-versions.sh
+++ update-versions.sh
@@ -54,7 +54,7 @@ update_pin() {
   echo "${version_variable}: $version" >&2
 }
 
-bats_tag="$(latest_github_tag bats-core/bats-core)"
+bats_tag="$(latest_github_tag bats-hardened/bats-core)"
 case "$bats_tag" in
   v*) bats_version="${bats_tag#v}" ;;
   *)
@@ -63,7 +63,7 @@ case "$bats_tag" in
     ;;
 esac
 bats_sha256="$(download_sha256 \
-  "https://github.com/bats-core/bats-core/archive/refs/tags/$bats_tag.tar.gz" \
+  "https://github.com/bats-hardened/bats-core/archive/refs/tags/$bats_tag.tar.gz" \
   "$temp_dir/bats-core.tar.gz")"
 update_pin BATS_VERSION BATS_SHA256 "$bats_version" "$bats_sha256"
 
